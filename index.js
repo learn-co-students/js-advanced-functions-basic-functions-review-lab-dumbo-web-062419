5a7db3c8bb20ae0ee55ebcf1fc8a4c209407ed02
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-11-05 16:41:02 -0500

    finished js-advanced functions lab, all tests passing

--- index.js
+++ index.js
@@ -6,3 +6,45 @@ function saturdayFun(activity="roller-skate") {
 let mondayWork = function(activity="go to the office") {
 	return `This Monday, I will ${activity}.`;
 }
+
+let wrapAdjective = function (flair="*") {
+
+	return function(adj="special"){
+		return `You are ${flair}${adj}${flair}!`
+	}
+}
+
+const Calculator = {
+
+	add: function (a, b) {
+
+		return a + b;
+	},
+
+	subtract: function(a, b) {
+
+		return a - b;
+	},
+	multiply: function(a, b) {
+
+		return a * b;
+	},
+	divide: function(a, b) {
+
+		return a / b;
+	}
+}
+
+function actionApplyer(starting, funkArray) {
+
+	if (funkArray.length == 0)
+		return starting;
+	else {
+		let result = starting;
+		for (var i = 0; i < funkArray.length; i ++) {
+
+			result = funkArray[i](result);
+		}
+		return result;
+	}
+} 
